59b5ea338ce7803987eb527a4c6d625363c29eaa
Rodolfo Win <rndcsjj@gmail.com>
2023-08-06 18:21:56 -0300

    FEAT: remove duplicidade na navbar nas paginas

--- resources/views/site/layout/basico.blade.php
+++ resources/views/site/layout/basico.blade.php
@@ -4,11 +4,10 @@
         <title>Super Gestão - @yield('titulo')</title>
         <meta charset="utf-8">
         <link rel="stylesheet" href=" {{ asset('css/estilo_basico.css') }} ">
-       
     </head>
 
     <body>
+        @include('site.layout._partials.topo')
         @yield('conteudo')
-
     </body>
 </html>
